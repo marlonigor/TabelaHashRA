f1207d0d34b4e12785b2360d4b95ac7baa3af402
marlonigor <66376336+marlonigor@users.noreply.github.com>
2023-10-13 23:15:52 +0000

    structures

---
+++ .ccls-cache/@home@runner@TabelaHashIgorMarlon/ra@structures.h
@@ -0,0 +1,23 @@
+
+
+#define tam 250
+
+struct matricula {
+  int RA;
+  char nome[81];
+  char email[41];
+  char turma;
+};
+typedef struct matricula MatAluno;
+typedef MatAluno* Hash[tam];
+
+int funcao_Esp(int RA);
+
+MatAluno* insere_Esp(Hash tab, int RA, char* name, char* mail, char turma);
+
+void remove_Esp(Hash tab, int RA);
+
+MatAluno* busca_Esp(Hash tab, int RA);
+
+int tamanho_Esp(Hash tab);
+
